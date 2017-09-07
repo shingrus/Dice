bf45a6e79c961bb0d9ba32c622224552213b56f4
shingrus <shingrus@gmail.com>
2017-09-07 15:38:10 +0300

    shake to roll

--- app/src/main/java/com/shingrus/dice/MainActivity.java
+++ app/src/main/java/com/shingrus/dice/MainActivity.java
@@ -1,10 +1,12 @@
 package com.shingrus.dice;
 
-import android.graphics.Color;
+    import android.content.Context;
+    import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+    import android.os.Vibrator;
+    import android.support.design.widget.FloatingActionButton;
+    import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -27,7 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> dice = new ArrayList<>(DICE_SIZE);
 
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+    private    TextView diceView ;
+    final Random random = new Random();
+    private Vibrator vibrator = null;
 
+    int rollingColor = 0;
+    int readyColor = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,70 +47,116 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final TextView diceView = (TextView) findViewById(R.id.dice);
-        final Random random = new Random();
+        diceView = (TextView) findViewById(R.id.dice);
 
-        final int rollingColor = getResources().getColor(R.color.diceRolling);
-        final int readyColor = getResources().getColor(R.color.diceReady);
-
+        rollingColor = getResources().getColor(R.color.diceRolling);
+        readyColor = getResources().getColor(R.color.diceReady);
 
         for (int i= 1 ; i<=DICE_SIZE; i++) {dice.add(String.format(Locale.US,"%d", i));}
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //start my own thread and
-                if (!diceThreadStarted) {
-                    diceThreadStarted = true;
-                    Log.d(LOG_TAG, "Start thread");
-
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Collections.shuffle(dice);
-
-                            int currentDiceRollIterations = DICE_ROLL_ITERATIONS - random.nextInt(DICE_ROLL_ITERATIONS/2);
-                            Log.d(LOG_TAG, "Thorow iterations: "  + Integer.toString(currentDiceRollIterations));
-                            for (int i = 0; i < currentDiceRollIterations; i++) {
-
-                                final String diceValue = dice.get(i%DICE_SIZE);
-
-                                if (i == 0) diceView.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        diceView.setTextColor(rollingColor);
-                                    }
-                                });
-
-                                diceView.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        diceView.setText(diceValue);
-                                    }
-                                });
-
-                                if (i == currentDiceRollIterations -1) diceView.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        diceView.setTextColor(readyColor);
-                                    }
-                                });
-
-                                try {
-                                    Thread.sleep(100 - i*2);
-                                } catch (InterruptedException e) {
-                                    Log.e(LOG_TAG, e.getMessage());
-                                }
-                            }
-                            Log.d(LOG_TAG,"Finish thread");
-                            diceThreadStarted = false;
-                        }
-                    }).start();
-                }
+                MainActivity.this.startRollingDice(false);
             }
         });
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+                Log.d(LOG_TAG, String.format(Locale.US,"Shake count: %d", count));
+
+                if (count >=2 )
+                    startRollingDice(true);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
+    /**
+     *
+     * @param isShake - boolean if shaked - enable vibro
+     */
+    private void startRollingDice(boolean isShake ) {
+        //start my own thread and
+        if (!diceThreadStarted) {
+            diceThreadStarted = true;
+            Log.d(LOG_TAG, "Start thread");
+            //TODO: fix vibration - doesn't work on my s8 :(
+//            if(isShake && vibrator != null ) {
+//                vibrator.vibrate(500);
+//                Log.d(LOG_TAG, "vibrate");
+//            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Collections.shuffle(dice);
+
+                    int currentDiceRollIterations = DICE_ROLL_ITERATIONS - random.nextInt(DICE_ROLL_ITERATIONS/2);
+                    Log.d(LOG_TAG, "Thorow iterations: "  + Integer.toString(currentDiceRollIterations));
+                    for (int i = 0; i < currentDiceRollIterations; i++) {
+
+                        final String diceValue = dice.get(i%DICE_SIZE);
+
+                        if (i == 0) diceView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                diceView.setTextColor(rollingColor);
+                            }
+                        });
+
+                        diceView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                diceView.setText(diceValue);
+                            }
+                        });
+
+                        if (i == currentDiceRollIterations -1) diceView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                diceView.setTextColor(readyColor);
+                            }
+                        });
+
+                        try {
+                            Thread.sleep(100 - i*2);
+                        } catch (InterruptedException e) {
+                            Log.e(LOG_TAG, e.getMessage());
+                        }
+                    }
+                    Log.d(LOG_TAG,"Finish thread");
+                    diceThreadStarted = false;
+                }
+            }).start();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/shingrus/dice/ShakeDetector.java
@@ -0,0 +1,72 @@
+package com.shingrus.dice;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * Created by shingrus on 07/09/2017.
+ * Tnaks to http://jasonmcreynolds.com/?p=388
+ */
+
+private class ShakeDetector implements SensorEventListener {
+
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 200;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 2000;
+
+    private OnShakeListener mListener;
+    private long mShakeTimestamp;
+    private int mShakeCount;
+
+    private void setOnShakeListener(OnShakeListener listener) {
+        this.mListener = listener;
+    }
+
+    private interface OnShakeListener {
+         void onShake(int count);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (mListener != null) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            float gX = x / SensorManager.GRAVITY_EARTH;
+            float gY = y / SensorManager.GRAVITY_EARTH;
+            float gZ = z / SensorManager.GRAVITY_EARTH;
+
+            // gForce will be close to 1 when there is no movement.
+            float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+
+
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                final long now = System.currentTimeMillis();
+                // ignore shake events too close to each other (500ms)
+                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                    return;
+                }
+
+                // reset the shake count after 3 seconds of no shakes
+                if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                    mShakeCount = 0;
+                }
+
+                mShakeTimestamp = now;
+                mShakeCount++;
+
+                mListener.onShake(mShakeCount);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
